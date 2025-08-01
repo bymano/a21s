421b6e47f84b8704ed52827f40a6130d8b9045ab
bymano <bymano@users.noreply.github.com>
2025-08-01 19:31:10 +0300

    Fix clock minute timing bug

--- scripts/home_screen/apps_functionality/clock/clock_functionality.js
+++ scripts/home_screen/apps_functionality/clock/clock_functionality.js
@@ -8,9 +8,9 @@ function rotateForCurrentTime() {
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  const minuteDeg = (minutes / 60) * 360;
-  const hourDeg = (hours / 12) * 360 + minuteDeg / 12;
   const secondDeg = (seconds / 60) * 360;
+  const minuteDeg = (minutes / 60) * 360 + secondDeg / 60;
+  const hourDeg = (hours / 12) * 360 + minuteDeg / 12;
 
   hourHandContainer.style.transform = `rotate(${hourDeg}deg)`;
   minuteHandContainer.style.transform = `rotate(${minuteDeg}deg)`;
